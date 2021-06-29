68a82464b151abea2115cdf0c4de673557528590
YauheniKhadanionak <ykhadanionak@exadel.com>
2021-06-29 18:57:19 +0300

    Add protonpack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { Card, Form, Layout, IconSearch, Icon } from "@abc/protonpack";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => (
+  <BrowserRouter>
+    <Layout>
+      <Layout.Header className="header">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="120"
+          height="64"
+          viewBox="0 0 120 64"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <text
+            fill="rgba(255,255,255,0.75)"
+            fontFamily="sans-serif"
+            fontSize="30"
+            dy="10.5"
+            fontWeight="bold"
+            x="50%"
+            y="50%"
+            textAnchor="middle"
+          >
+            LOGO
+          </text>
+        </svg>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "minmax(auto, 24rem) auto",
+            alignItems: "center",
+            justifyContent: "space-between",
+            color: "white",
+            gridGap: "2rem",
+          }}
+        >
+          <Form.Input
+            placeholder="Select Organization"
+            before={<IconSearch />}
+            className="input"
+            name="select"
+            abcId="select"
+            id="select"
+            outlined
+          />
+        </div>
+      </Layout.Header>
+      <Layout.SideMenu />
+      <Layout.NavBar items={navItems} />
+      <Layout.Content className="grid">
+        <Card abcId="content1" shadow={false} className="content content1">
+          1
+        </Card>
+        <Card abcId="content2" shadow={false} className="content content2">
+          2
+        </Card>
+        <Card abcId="content3" shadow={false} className="content content3">
+          3
+        </Card>
+        <Card abcId="content4" shadow={false} className="content content4">
+          4
+        </Card>
+        <Card abcId="content5" shadow={false} className="content content5">
+          5
+        </Card>
+      </Layout.Content>
+    </Layout>
+  </BrowserRouter>
+);
+
+const navItems = [
+  { path: "2", label: "Business", key: "Business" },
+  { path: "3", label: "Members", key: "Members" },
+  { path: "4", label: "Staff", key: "Staff" },
+  { path: "5", label: "Brand", key: "Brand" },
+  { path: "7", label: "Reporting", key: "Reporting" },
+];
 
 export default App;
