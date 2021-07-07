09081ff3d330ffbd0af14b4baf69cda9a2ae207a
Aliaksei Aliakseyeu <aaliakseyeu@exadel.com>
2021-07-07 09:28:22 -0500

    Footer section

--- src/editor/sections/about/index.jsx
+++ src/editor/sections/about/index.jsx
@@ -1,29 +1,30 @@
-import { Button, Text, Header } from '@abc/protonpack'
-import { SectionBackground } from '../../components/SectionBackground'
+import {Button, Text, Header} from '@abc/protonpack'
+import {SectionBackground} from '../../components/SectionBackground'
 import './styles.css'
 
-export const About = ({ content, settings }) => (
-  <section className="sectionAbout">
-    <SectionBackground src={content.background} />
-    <div className="sectionAboutContent">
-      <Header className="sectionAboutTitle" color="cardPrimary">
-        {content.title}
-      </Header>
-      <br />
-      <br />
-      <br />
-      <Text className="sectionAboutDescription" color="cardPrimary">
-        {content.description}
-      </Text>
-      <br />
-      <br />
-      <br />
-      <Button className="sectionAboutButton" uppercase>
-        Join
-      </Button>
-    </div>
-  </section>
+export const About = ({content, settings}) => (
+    <section className="sectionAbout">
+        <SectionBackground src={content.background}/>
+        <div className="sectionAboutContent">
+            <Header className="sectionAboutTitle" color="cardPrimary">
+                {content.title}
+            </Header>
+            <br/>
+            <br/>
+            <br/>
+            <Text className="sectionAboutDescription" color="cardPrimary">
+                {content.description}
+            </Text>
+            <br/>
+            <br/>
+            <br/>
+            <Button className="sectionAboutButton" uppercase
+                    onClick={() => document.querySelector('#join').scrollIntoView({behavior: 'smooth'})}>
+                        Join
+            </Button>
+        </div>
+    </section>
 )
 
-export { AboutContentForm } from './contentForm'
-export { AboutSettingsForm } from './settingsForm'
+export {AboutContentForm} from './contentForm'
+export {AboutSettingsForm} from './settingsForm'
